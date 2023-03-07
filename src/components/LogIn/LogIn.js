60796e5b3c9078f697ca2585994695557f892b0f
Tushar-Kanti-Dev <62166306+Tushar-Kanti-Dev@users.noreply.github.com>
2023-03-07 19:11:32 +0600

    used another firebase system

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,15 +1,19 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import app from '../../firebase.init';
 import './LogIn.css'
 
+const auth = getAuth(app)
 const LogIn = () => {
-    const {user, signInWithGoogle} = useFirebase()
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth)
+    
     return (
         <div className='login-container'>
             <h2>LogIn Please </h2>
             <div className="google-signin">
                 
-                <button onClick={signInWithGoogle}>Google SignIn</button>
+                <button onClick={()=> signInWithGoogle()}>Google SignIn</button>
             </div>
 
             <form action="">
